a2cefd0f449b0d9dbc42ff0e8f7eb3ef20c1634b
坂田一真 <kazuma@sakatakazumanoMac-mini.local>
2021-05-19 07:33:56 +0900

    API(MVVM)

---
+++ Qiita(MVVC)/Model/Qiita.swift
@@ -0,0 +1,8 @@
+//
+//  Qiita.swift
+//  Qiita(MVVC)
+//
+//  Created by 坂田一真 on 2021/05/19.
+//
+
+import Foundation
---
+++ Qiita(MVVC)/View/infoCell.swift
@@ -0,0 +1,8 @@
+//
+//  infoCell.swift
+//  Qiita(MVVC)
+//
+//  Created by 坂田一真 on 2021/05/19.
+//
+
+import Foundation
